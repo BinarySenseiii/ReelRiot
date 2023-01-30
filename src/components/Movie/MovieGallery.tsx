fb9d8b3d046609439b2ea345b7b52018cfe7d381
BloodySteps <codewithtyson@gmail.com>
2023-01-30 10:19:21 +0400

    🐞 fix: movie--detail--paghe

--- src/components/Movie/MovieGallery.tsx
+++ src/components/Movie/MovieGallery.tsx
@@ -1,31 +1,28 @@
-import React from 'react';
-import { SimpleGrid, Skeleton, Title } from '@mantine/core';
+import { SimpleGrid, Title } from '@mantine/core';
 import Image from 'next/legacy/image';
+import React from 'react';
 
 type MovieGalleryProps = {
   galleryImages: string[] | undefined;
-  isLoading: boolean;
 };
 
-const MovieGallery: React.FC<MovieGalleryProps> = ({ galleryImages, isLoading }) => (
+const MovieGallery: React.FC<MovieGalleryProps> = ({ galleryImages }) => (
   <>
     <Title order={3} mt="lg">
       Gallery
     </Title>
 
     <SimpleGrid cols={3} spacing="xs" mt="sm">
-      {isLoading
-        ? Array.from({ length: 3 }, (_, i) => <Skeleton key={i} visible={isLoading} height={100} />)
-        : galleryImages?.map((imageSrc) => (
-            <Image
-              key={imageSrc}
-              src={imageSrc}
-              width={400}
-              height={200}
-              layout="responsive"
-              alt="movie screen shot not found"
-            />
-          ))}
+      {galleryImages?.map((imageSrc) => (
+        <Image
+          key={imageSrc}
+          src={imageSrc}
+          width={400}
+          height={200}
+          layout="responsive"
+          alt="movie screen shot not found"
+        />
+      ))}
     </SimpleGrid>
   </>
 );
