a789868a1ada50b54b3390d5ba75f6b3a9e50b2f
BloodySteps <codewithtyson@gmail.com>
2023-01-30 16:13:12 +0400

    feat: tmdb__api

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,12 +1,26 @@
-/* eslint-disable consistent-return */
 import axios from 'axios';
 
-export const client = axios.create({
+export const ytsClient = axios.create({
   baseURL: 'https://yts.mx/api/v2',
 });
 
-export const fetch = async (endpoint: string) => {
-  const response = await client.get(endpoint);
+export const tmdbClient = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+});
+
+export const ytsFetch = async (endpoint: string) => {
+  const response = await ytsClient.get(endpoint);
   const { data } = await response.data;
+  return data;
+};
+
+export const tmdbFetch = async (endpoint: string, params: any) => {
+  const response = await tmdbClient.get(endpoint, {
+    headers: {
+      Authorization: `bearer ${process.env.TMDB_TOKEN}`,
+    },
+    params,
+  });
+  const data = await response.data;
   return data;
 };
