2a9d6c0fe18543cea76a4aec5905b729637bac80
Faisal TariQ <68985954+FrontendFrenzy@users.noreply.github.com>
2023-02-08 23:20:58 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -16,6 +16,14 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  
+    typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 
   // SVGR
   webpack(config) {
