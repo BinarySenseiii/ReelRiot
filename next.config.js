b02a29737f21f0dd2b2412246bad33ce94995b85
Faisal TariQ <68985954+FrontendFrenzy@users.noreply.github.com>
2023-02-08 23:16:55 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
 
   images: {
     domains: ['yts.mx', 'image.tmdb.org'],
+  }, 
+  
+    eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 
   // SVGR
