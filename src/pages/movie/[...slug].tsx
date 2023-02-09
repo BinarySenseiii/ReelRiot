0824bd8f9962f93f362473ae2dcfd201c2a1b03e
Faisal Tariq <codewithtyson@gmail.com>
2023-02-09 10:48:11 +0400

    refactor: :fire: movie-detail

--- src/pages/movie/[...slug].tsx
+++ src/pages/movie/[...slug].tsx
@@ -9,42 +9,31 @@ import {
   Button,
   Grid,
   Group,
-  Overlay,
-  Paper,
-  Stack,
+  Spoiler,
+  Tabs,
   Text,
   Title,
 } from '@mantine/core';
-import { useQuery } from '@tanstack/react-query';
+import {
+  IconArrowLeft,
+  IconDownload,
+  IconMessageCircle,
+  IconPhoto,
+  IconVideo,
+} from '@tabler/icons';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
-import { IconDownload } from '@tabler/icons';
-import {
-  IImages,
-  IMovie,
-  IReviewResult,
-  ITmdbMovieResult,
-  IVideoResult,
-} from '@/types/Movie.types';
-import {
-  getBackdropImage,
-  getMovieImages,
-  getMovieReviews,
-  getMovieVideos,
-  getPosterImage,
-  getTmdbMovieDetail,
-  movieDetailUrl,
-} from '@/services/config';
+import Link from 'next/link';
+import { IImages, IMovie, IReviewResult, ITmdbMovie, IVideoResult } from '@/types/Movie.types';
+import { getPosterImage, getTmdbMovieDetail, movieDetailUrl } from '@/services/config';
 import { tmdbFetch, ytsFetch } from '@/services/client';
 
-import pb from '@/assets/bg.jpg';
 import poster from '@/assets/blur.jpg';
-import { MovieTorrents, useMovieStyles } from '@/components/Movie';
+import { MovieCast, MovieTorrents } from '@/components/Movie';
 import { Container } from '@/components/ui';
 
 interface IMovieDetailProps {
   movie: IMovie;
-  tmdbMovie: ITmdbMovieResult;
+  tmdbMovie: ITmdbMovie;
   images: IImages;
   reviews: IReviewResult;
   videos: IVideoResult;
@@ -56,158 +45,150 @@ const MovieDetail: React.FC<IMovieDetailProps> = ({
   // images,
   // reviews,
   // videos,
-}) => {
-  const [isMovie, setIsMovie] = useState<boolean>();
-  const { classes } = useMovieStyles();
+}) => (
+  // const { classes } = useMovieStyles();
 
-  useEffect(() => setIsMovie(tmdbMovie.movie_results.length > 0), []);
+  // const images = useQuery(
+  //   [`${movie.title_english}/images`],
+  //   () => tmdbFetch(getMovieImages(tmdbMovie.id)),
+  //   {
+  //     enabled: !!tmdbMovie,
+  //   }
+  // );
 
-  const images = useQuery(
-    [`${movie.title_english}/images`],
-    () => tmdbFetch(getMovieImages(tmdbMovie.movie_results[0].id)),
-    {
-      enabled: isMovie,
-    }
-  );
+  // const videos = useQuery(
+  //   [`${movie.title_english}/videos`],
+  //   () => tmdbFetch(getMovieVideos(tmdbMovie.id)),
+  //   {
+  //     enabled: !!tmdbMovie,
+  //   }
+  // );
 
-  const videos = useQuery(
-    [`${movie.title_english}/videos`],
-    () => tmdbFetch(getMovieVideos(tmdbMovie.movie_results[0].id)),
-    {
-      enabled: isMovie,
-    }
-  );
+  // const reviews = useQuery(
+  //   [`${movie.title_english}/reviews`],
+  //   () => tmdbFetch(getMovieReviews(tmdbMovie.id)),
+  //   {
+  //     enabled: !!tmdbMovie,
+  //   }
+  // );
 
-  const reviews = useQuery(
-    [`${movie.title_english}/reviews`],
-    () => tmdbFetch(getMovieReviews(tmdbMovie.movie_results[0].id)),
-    {
-      enabled: isMovie,
-    }
-  );
-
-  console.log(images, reviews, videos);
-
-  return (
-    <>
-      <Box className={classes.backgroundContainer}>
-        <Image
-          src={
-            isMovie
-              ? getBackdropImage(tmdbMovie.movie_results[0].backdrop_path)
-              : movie.background_image
-          }
-          alt={`${movie.title_long} background not found`}
-          fill
-          className={classes.backgroundImage}
-          placeholder="blur"
-          blurDataURL={pb.blurDataURL}
-        />
-
-        <Overlay opacity={0.6} color="#000" blur={2} />
-      </Box>
-
-      <Container>
-        <Paper
-          shadow="sm"
-          p="md"
-          withBorder
-          mt="-48px"
-          component="main"
-          className={classes.mainWrapper}
-        >
-          <Grid>
-            <Grid.Col span={4}>
-              <AspectRatio ratio={720 / 1080}>
-                <Image
-                  src={
-                    isMovie
-                      ? getPosterImage(tmdbMovie.movie_results[0].poster_path)
-                      : movie.large_cover_image
-                  }
-                  fill
-                  alt={`${movie.title_english} poster not found`}
-                  placeholder="blur"
-                  blurDataURL={poster.blurDataURL}
-                  sizes="(max-width: 768px) 100vw,
+  <Container mt="xl">
+    <Link href="/browse">
+      <Button mb="xl" leftIcon={<IconArrowLeft size={14} />} size="xs" variant="light" color="gray">
+        Back
+      </Button>
+    </Link>
+    <Box component="main">
+      <Grid>
+        <Grid.Col span={4}>
+          <AspectRatio ratio={720 / 1080}>
+            <Image
+              src={tmdbMovie ? getPosterImage(tmdbMovie.poster_path) : movie.large_cover_image}
+              fill
+              alt={`${movie.title_english} poster not found`}
+              placeholder="blur"
+              blurDataURL={poster.blurDataURL}
+              sizes="(max-width: 768px) 100vw,
                   (max-width: 1200px) 50vw,
                   33vw"
-                />
-              </AspectRatio>
-            </Grid.Col>
-            <Grid.Col span={8}>
-              <Title color="white">{movie.title_english ?? tmdbMovie.movie_results[0].title}</Title>
+            />
+          </AspectRatio>
+        </Grid.Col>
+        <Grid.Col span={8}>
+          <Title color="white">{tmdbMovie ? tmdbMovie.title : movie.title_english}</Title>
 
-              <Group spacing={6} mt="sm">
-                {movie.genres.map((genre) => (
-                  <Badge key={genre} variant="filled">
-                    <Text color="white">{genre}</Text>
-                  </Badge>
-                ))}
-              </Group>
+          <Group spacing={6} mt="sm">
+            {movie.genres.map((genre) => (
+              <Badge key={genre} variant="filled">
+                <Text color="white">{genre}</Text>
+              </Badge>
+            ))}
+          </Group>
 
-              <Group spacing="xs" mt="sm">
-                <Text fz="md">
-                  <Text component="strong">Release Date: </Text>
-                  {isMovie ? tmdbMovie.movie_results[0].release_date : movie.date_uploaded}
-                </Text>
+          <Group spacing="xs" mt="sm">
+            <Text fz="md">
+              <Text component="strong">Release Year: </Text>
+              {tmdbMovie ? tmdbMovie.release_date : movie.year}
+            </Text>
 
-                <Text fz="md">
-                  <Text component="strong">Runtime: </Text>
-                  {movie.runtime} Mins
-                </Text>
-                <Text fz="md">
-                  <Text component="strong">Language: </Text>
-                  {movie.language}
-                </Text>
-              </Group>
+            <Text fz="md">
+              <Text component="strong">Runtime: </Text>
+              {movie.runtime === 0 ? '120' : movie.runtime} Mins
+            </Text>
+            <Text fz="md">
+              <Text component="strong">Language: </Text>
+              {tmdbMovie ? tmdbMovie.original_language : movie.language}
+            </Text>
+          </Group>
 
-              <MovieTorrents torrents={movie.torrents} isTitle />
+          <MovieTorrents torrents={movie.torrents} isTitle />
 
-              <Text size="xs" mt={8}>
-                WEB: same quality as BluRay
-              </Text>
+          <Text size="xs" mt={8}>
+            WEB: same quality as BluRay
+          </Text>
 
-              <Anchor
-                href={`https://yifysubtitles.org/movie-imdb/${movie.imdb_code}`}
-                target="_blank"
-              >
-                <Button
-                  size="xs"
-                  mt="lg"
-                  sx={(theme) => ({
-                    background: 'white',
-                    color: theme.colors.dark[9],
-                    '&:hover': { background: theme.colors.dark[0] },
-                  })}
-                  leftIcon={<IconDownload size={20} />}
-                >
-                  Download Subtitles
-                </Button>
-              </Anchor>
+          <Group mt="lg" spacing="xs">
+            <Anchor
+              href={`https://yifysubtitles.org/movie-imdb/${movie.imdb_code}`}
+              target="_blank"
+            >
+              <Button size="xs" variant="white" color="dark" leftIcon={<IconDownload size={16} />}>
+                Download Subtitles
+              </Button>
+            </Anchor>
 
-              <Stack spacing={2} mt="md">
-                <Title order={2}>Overview</Title>
+            <Anchor href={` https://www.imdb.com/title/${movie.imdb_code}`} target="_blank">
+              <Button size="xs" color="yellow">
+                <Text color="dark">View Details On IMDB</Text>
+              </Button>
+            </Anchor>
+          </Group>
+
+          <MovieCast isTitle casts={movie.cast} />
+
+          <Tabs defaultValue="gallery" mt="xl">
+            <Tabs.List>
+              <Tabs.Tab value="gallery" icon={<IconMessageCircle size={14} />}>
+                Overview
+              </Tabs.Tab>
+              <Tabs.Tab value="messages" icon={<IconPhoto size={14} />}>
+                Images
+              </Tabs.Tab>
+              <Tabs.Tab value="settings" icon={<IconVideo size={14} />}>
+                Videos
+              </Tabs.Tab>
+            </Tabs.List>
+
+            <Tabs.Panel value="gallery" pt="xs">
+              <Spoiler maxHeight={110} showLabel="Show more" hideLabel="Hide">
                 <Text color="white" fz="sm">
-                  {isMovie ? tmdbMovie.movie_results[0].overview : movie.description_full}
+                  {movie.description_full}
                 </Text>
-              </Stack>
-            </Grid.Col>
-          </Grid>
-        </Paper>
-      </Container>
-    </>
-  );
-};
+              </Spoiler>
+            </Tabs.Panel>
+
+            <Tabs.Panel value="messages" pt="xs">
+              Messages tab content
+            </Tabs.Panel>
+
+            <Tabs.Panel value="settings" pt="xs">
+              Settings tab content
+            </Tabs.Panel>
+          </Tabs>
+        </Grid.Col>
+      </Grid>
+    </Box>
+  </Container>
+);
 export default MovieDetail;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const [movieId] = context.query.slug as string[];
 
   const { movie } = await ytsFetch(movieDetailUrl(movieId));
-  const tmdbMovie = await tmdbFetch(getTmdbMovieDetail(movie.imdb_code));
+  const { movie_results } = await tmdbFetch(getTmdbMovieDetail(movie.imdb_code));
 
   return {
-    props: { movie, tmdbMovie },
+    props: { movie, tmdbMovie: movie_results.length > 0 ? movie_results[0] : null },
   };
 };
