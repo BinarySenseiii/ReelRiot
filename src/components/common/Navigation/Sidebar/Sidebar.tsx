773724fa95c610284a18d464dbc72310bf958e4e
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-01 22:22:48 +0000

    update sidebar

--- src/components/common/Navigation/Sidebar/Sidebar.tsx
+++ src/components/common/Navigation/Sidebar/Sidebar.tsx
@@ -3,13 +3,17 @@ import React from 'react';
 import Link from 'next/link';
 import { Title, Box } from '@mantine/core';
 import { useStyles } from '../Navigation.styled';
-import { Menu } from '@/components/common/Navigation/Sidebar/sidebarData';
+import { General, Menu, Social } from '@/components/common/Navigation/Sidebar/sidebarData';
 
 function Sidebar() {
   const { classes } = useStyles();
 
   return (
-    <div style={{ padding: '1rem' }}>
+    <div
+      style={{
+        padding: '1rem',
+      }}
+    >
       <Link href="/" passHref>
         <span className={classes.logo}>
           <Title color="brand">
@@ -20,9 +24,34 @@ function Sidebar() {
           </Title>
         </span>
       </Link>
-      <div>
+      <div style={{ marginTop: '1rem', display: 'flex', flexDirection: 'column' }}>
         <ul style={{ padding: '0' }} className={classes.unorderedList}>
+          <span>Menu</span>
           {Menu.map((data) => (
+            <li key={data.name} className={classes.navlist}>
+              <Link href={`/${data.name}`} className={classes.navLinks}>
+                {data.icon}
+                {data.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <ul style={{ padding: '0' }} className={classes.unorderedList}>
+          <span>Social</span>
+          {Social.map((data) => (
+            <li key={data.name} className={classes.navlist}>
+              <Link href={`/${data.name}`} className={classes.navLinks}>
+                {data.icon}
+                {data.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <ul style={{ padding: '0' }} className={classes.unorderedList}>
+          <span>General</span>
+          {General.map((data) => (
             <li key={data.name} className={classes.navlist}>
               <Link href={`/${data.name}`} className={classes.navLinks}>
                 {data.icon}
