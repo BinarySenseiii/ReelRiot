38dd0b0cf1ab9da9d82549be85496b4278e677c3
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-02 13:32:33 +0000

    Fix responsiveness

--- src/layout/MasterLayout.tsx
+++ src/layout/MasterLayout.tsx
@@ -2,8 +2,9 @@
 import styled from '@emotion/styled';
 import React, { ReactNode } from 'react';
 import NextNProgress from 'nextjs-progressbar';
-// import { ClassNames } from '@emotion/react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { Title, Box } from '@mantine/core';
 import Footer from '../components/common/Footer';
 import Navigation from '../components/common/Navigation';
 import { links } from '../components/common/Navigation/Navigation.mock';
@@ -11,7 +12,6 @@ import ScrollToTop from '@/components/ScrollToTop';
 import Sidebar from '@/components/common/Navigation/Sidebar/Sidebar';
 import { useStyles } from '../components/common/Navigation/Navigation.styled';
 
-import useResize from '@/Hooks/useResize';
 import SidebarTrending from '@/components/common/Navigation/Sidebar/Sidebar_Trending';
 
 const LayoutWrapper = styled.div`
@@ -21,10 +21,30 @@ const LayoutWrapper = styled.div`
   width: 100%;
   max-width: 1400px;
   margin-inline: auto;
-  padding-top: 3rem;
+  padding-top: 2rem;
 
   #__root {
     padding-bottom: 2rem;
+  }
+
+  @media (max-width: 400px) {
+    grid-template-columns: 50px auto !important;
+  }
+  @media (max-width: 600px) {
+    grid-template-columns: 70px auto !important;
+  }
+  @media (max-width: 768px) {
+    grid-templatet-columns: 200px auto !important;
+    padding-top: 0 !important;
+  }
+`;
+
+const ShowLogo = styled.div`
+  display: none;
+  @media (max-width: 600px) {
+    display: block;
+    text-align: center;
+    margin-top: 1rem;
   }
 `;
 
@@ -34,7 +54,6 @@ type MasterLayoutProps = {
 
 const MasterLayout: React.FC<MasterLayoutProps> = ({ children }) => {
   const { classes } = useStyles();
-  const { size } = useResize();
   const router = useRouter();
   const { pathname } = router;
 
@@ -42,16 +61,26 @@ const MasterLayout: React.FC<MasterLayoutProps> = ({ children }) => {
     <>
       <ScrollToTop />
       <NextNProgress color="#dd0913" />
+      <ShowLogo>
+        <Link href="/" passHref>
+          <span className={classes.logo}>
+            <Title color="brand">
+              REEL
+              <Box component="span" sx={{ color: 'white' }}>
+                RIOT
+              </Box>
+            </Title>
+          </span>
+        </Link>
+      </ShowLogo>
       <LayoutWrapper
         style={
           pathname === '/browse'
             ? { gridTemplateColumns: '200px auto' }
-            : size <= 768
-            ? { gridTemplateColumns: '200px auto' }
-            : { gridTemplateColumns: '200px auto 150px' }
+            : { gridTemplateColumns: '200px auto 150px', paddingTop: '2rem' }
         }
       >
-        <aside className={classes.aside}>
+        <aside>
           <Sidebar />
         </aside>
 
@@ -60,7 +89,7 @@ const MasterLayout: React.FC<MasterLayoutProps> = ({ children }) => {
           <div id="__root">{children}</div>
           <Footer />
         </div>
-        <aside className={classes.aside_trending}>
+        <aside>
           <SidebarTrending />
         </aside>
       </LayoutWrapper>
