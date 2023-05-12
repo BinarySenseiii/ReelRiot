12d06564785221888e4727d111d58687357ae3e0
Faisal Tariq <codewithtyson@gmail.com>
2023-05-12 11:10:29 +0400

    🐞 fix: typo _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,7 +19,6 @@ const DefaultLayout = dynamic(() =>
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
-  u;
 };
 
 type AppPropsWithLayout = AppProps & {
