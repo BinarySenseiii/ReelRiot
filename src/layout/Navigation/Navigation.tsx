7dc47fc963a6edc7ec88895896adfb5693599da0
Faisal Tariq <codewithtyson@gmail.com>
2023-08-04 00:08:15 +0400

    ....

--- src/layout/Navigation/Navigation.tsx
+++ src/layout/Navigation/Navigation.tsx
@@ -1,60 +1,51 @@
-import {
-  Box,
-  Burger,
-  Group,
-  Header,
-  Paper,
-  Title,
-  Transition,
-} from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import Link from 'next/link';
-import React from 'react';
-import { HEADER_HEIGHT, useStyles } from './Navigation.styled';
+import { Box, Burger, Group, Header, Paper, Transition } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import React from 'react'
+import { HEADER_HEIGHT, useStyles } from './Navigation.styled'
 
-import { Container } from '@/components/ui';
-import NavLink from './NavLink';
-import { menuLinks } from '@/mock';
-import Logo from '../Logo';
+import { Container } from '@/components/ui'
+import { menuLinks } from '@/mock'
+import Logo from '../Logo'
+import NavLink from './NavLink'
 
 const Navigation: React.FC = () => {
-  const [opened, { toggle, close }] = useDisclosure(false);
-  const { classes } = useStyles();
+	const [opened, { toggle, close }] = useDisclosure(false)
+	const { classes } = useStyles()
 
-  const items = menuLinks.map((link) => (
-    <NavLink key={link.label} onClose={close} {...link} />
-  ));
+	const items = menuLinks.map(link => (
+		<NavLink key={link.label} onClose={close} {...link} />
+	))
 
-  return (
-    <Header height={HEADER_HEIGHT} mb={120} className={classes.root}>
-      <Container>
-        <Box className={classes.header}>
-          <Logo />
+	return (
+		<Header height={HEADER_HEIGHT} mb={120} className={classes.root}>
+			<Container>
+				<Box className={classes.header}>
+					<Logo />
 
-          <Group spacing={5} className={classes.links}>
-            {items}
-          </Group>
-          <Burger
-            opened={opened}
-            onClick={toggle}
-            className={classes.burger}
-            size="sm"
-            aria-label="toggle menu"
-          />
-          <Transition
-            transition="pop-top-right"
-            duration={200}
-            mounted={opened}
-          >
-            {(styles) => (
-              <Paper className={classes.dropdown} withBorder style={styles}>
-                {items}
-              </Paper>
-            )}
-          </Transition>
-        </Box>
-      </Container>
-    </Header>
-  );
-};
-export default Navigation;
+					<Group spacing={5} className={classes.links}>
+						{items}
+					</Group>
+					<Burger
+						opened={opened}
+						onClick={toggle}
+						className={classes.burger}
+						size="sm"
+						aria-label="toggle menu"
+					/>
+					<Transition
+						transition="pop-top-right"
+						duration={200}
+						mounted={opened}
+					>
+						{styles => (
+							<Paper className={classes.dropdown} withBorder style={styles}>
+								{items}
+							</Paper>
+						)}
+					</Transition>
+				</Box>
+			</Container>
+		</Header>
+	)
+}
+export default Navigation
